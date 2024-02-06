a7c44003dc6410319d5701372c8871961deb7021
racoelhosilva <racoelhosilva@gmail.com>
2024-02-06 21:09:10 +0000

    2015 Day 19: Medicine for Rudolph

---
+++ 2015/day19/main.py
@@ -0,0 +1,64 @@
+# Advent of Code 2015
+# Day 19: Medicine for Rudolph
+# https://adventofcode.com/2015/day/19
+
+import re, collections
+
+def a(data):
+    helper = dict()
+    target = data[-1]
+    for line in data[:-2]:
+        fields = line.split(" ")
+        if fields[0] in helper.keys():
+            helper[fields[0]] = helper[fields[0]] + [fields[-1]]
+        else:
+            helper[fields[0]] = [fields[-1]] 
+
+    results = set()
+    for key, value in helper.items():
+        for match in re.finditer(key, target):
+            for rep in value:
+                new = target[0:match.start()] + rep + target[match.end():]
+                results.add(new)
+
+    return len(results)
+
+def b(data):  
+    helper = dict()
+    target = data[-1]
+    for line in data[:-2]:
+        fields = line.split(" ")
+        if fields[0] in helper.keys():
+            helper[fields[0]] = helper[fields[0]] + [fields[-1]]
+        else:
+            helper[fields[0]] = [fields[-1]] 
+    
+    counter = 0
+    while target != "e":
+        shortest_len = float('inf')
+        shortest = target
+        for key, value in helper.items():
+            for option in value:
+                for match in re.finditer(option, target):
+                    undo = target[:match.start()] + key + target[match.end():]
+                    if len(undo) < shortest_len:
+                        shortest_len = len(undo)
+                        shortest = undo
+        target = shortest
+        counter += 1
+    return counter   
+
+def test():
+    print()
+    print("Test 1:", "Passed" if a(["H => HO","H => OH","O => HH", "", "HOH"]) == 4 else "Failed")
+    print("Test 2:", "Passed" if a(["H => HO","H => OH","O => HH", "", "HOHOHO"]) == 7 else "Failed")
+    print("Test 3:", "Passed" if b(["e => H","e => O","H => HO","H => OH","O => HH", "", "HOH"]) == 3 else "Failed")
+    print("Test 4:", "Passed" if b(["e => H","e => O","H => HO","H => OH","O => HH", "", "HOHOHO"]) == 6 else "Failed")
+    print()
+
+if __name__ == "__main__":
+    print("\tAdvent of Code 2015\nDay 19: Medicine for Rudolph")
+    data = open("/home/rodrigo/Documents/projects/adventOfCode/2015/day19/input.txt", "r").read().splitlines()
+    test()
+    print("Part a:", a(data))
+    print("Part b:", b(data))
